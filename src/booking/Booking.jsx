d84b98f946268536c651ba04281ce26529157eef
smhulme <smhulme101@gmail.com>
2025-10-21 19:53:53 -0600

    fixed booking to be able to show calendly on safari

--- src/booking/Booking.jsx
+++ src/booking/Booking.jsx
@@ -4,13 +4,15 @@ import './booking.css';
 
 export default function Booking() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://assets.calendly.com/assets/external/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
+    if (!window.Calendly) {
+      const script = document.createElement('script');
+      script.src = "https://assets.calendly.com/assets/external/widget.js";
+      script.async = true;
+      document.body.appendChild(script);
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
   }, []);
 
   const navigate = useNavigate();
